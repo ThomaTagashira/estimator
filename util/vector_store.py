c24b051b677f10d58c254a9977586412f3deee13
ThomaTagashira <140862101+ThomaTagashira@users.noreply.github.com>
2024-08-24 14:58:02 -0700

    asdfassdgasdfga

--- util/vector_store.py
+++ util/vector_store.py
@@ -1,9 +1,14 @@
 import os
 from langchain_community.vectorstores import PGVector
 from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+connection_strings = os.getenv('TEST_CONNECTION_STRING', os.getenv('CONNECTION_STRING'))
+collection_names = os.getenv('TEST_COLLECTION_NAME', os.getenv('COLLECTION_NAME'))
 
 db = PGVector(
-    connection_string=os.environ['CONNECTION_STRING'],
+    connection_string=connection_strings,
     embedding_function=OpenAIEmbeddings(),
-    collection_name=os.environ['COLLECTION_NAME'],
+    collection_name=collection_names,
 )
