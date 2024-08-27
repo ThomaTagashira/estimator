c5aac90eb26377d0f5bf31477be6c614ddc287b0
ThomaTagashira <140862101+ThomaTagashira@users.noreply.github.com>
2024-08-27 13:05:40 -0700

    fine tuning deployment scripts

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,8 +1,18 @@
-#validate_service.sh
+# validate_service.sh
 
-if curl -f http://localhost; then
+# Check the environment variable to determine which URL to validate
+if [ "$ENVIRONMENT" == "Dev" ]; then
+    URL="http://localhost"
+else
+    URL="https://thomatagashira.com"
+fi
+
+echo "Validating service at $URL..."
+
+# Perform the validation
+if curl -f $URL; then
     echo "Service is running successfully."
 else
-    echo "Service validation failed." >&2
+    echo "Service validation failed at $URL." >&2
     exit 1
 fi
