481b47ad9f90c081d693be2732db9f1d6b8c22eb
Goblin_Lord <140862101+ThomaTagashira@users.noreply.github.com>
2024-11-15 15:27:14 -0800

    asdf

--- util/vector_store.py
+++ util/vector_store.py
@@ -1,14 +1,18 @@
-import os
+from sqlalchemy import create_engine
 from langchain_postgres.vectorstores import PGVector
 from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-connection_strings = os.getenv('TEST_CONNECTION_STRING', os.getenv('CONNECTION_STRING'))
-collection_names = os.getenv('TEST_COLLECTION_NAME', os.getenv('COLLECTION_NAME'))
+
+connection_string = os.getenv('TEST_CONNECTION_STRING', os.getenv('CONNECTION_STRING'))
+collection_name = os.getenv('TEST_COLLECTION_NAME', os.getenv('COLLECTION_NAME'))
+
+engine = create_engine(connection_string)
 
 db = PGVector(
-    connection_string=connection_strings,
+    connection=engine.connect(),
     embedding_function=OpenAIEmbeddings(),
-    collection_name=collection_names,
+    collection_name=collection_name,
 )
