ce04e97bcf480cf60386773aeafde5b23aa699b7
Goblin_Lord <140862101+ThomaTagashira@users.noreply.github.com>
2024-12-23 18:02:55 -0800

    altered URL from str: to int
    modified unittests to align with changes

--- api/tests/test_delete_user_estimate_view.py
+++ api/tests/test_delete_user_estimate_view.py
@@ -9,17 +9,17 @@ class DeleteEstimateAPITest(TestCase):
         self.client = APIClient()
         self.client.login(username='testuser', password='testpassword')
 
-        self.estimate = UserEstimates.objects.create(user=self.user, estimate_id='EST-0001')
+        self.estimate = UserEstimates.objects.create(user=self.user, estimate_id=1)
 
     def test_delete_user_estimate(self):
         response = self.client.delete(f'/api/delete-estimate/{self.estimate.estimate_id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(UserEstimates.objects.filter(estimate_id='EST-0001').exists())
+        self.assertFalse(UserEstimates.objects.filter(estimate_id=1).exists())
 
     def test_delete_estimate_not_owned(self):
         other_user = User.objects.create_user(username='otheruser', password='otherpassword')
-        other_estimate = UserEstimates.objects.create(user=other_user, estimate_id='EST-0002')
+        other_estimate = UserEstimates.objects.create(user=other_user, estimate_id=2)
 
         response = self.client.delete(f'/api/delete-estimate/{other_estimate.estimate_id}/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        self.assertTrue(UserEstimates.objects.filter(estimate_id='EST-0002').exists())
+        self.assertTrue(UserEstimates.objects.filter(estimate_id=2).exists())
